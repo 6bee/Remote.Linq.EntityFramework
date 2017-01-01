7b34673b7b87bf7e16e156c7f9a740cb8361aa7a
Christof Senn <christof.senn@gmail.com>
2017-01-02 00:08:49 +0100

    updated to dotnet cli 1.0.0-preview2

--- pack.bat
+++ pack.bat
@@ -1,1 +1,1 @@
-dnu restore && dnu build src\Remote.Linq.EntityFramework test\Remote.Linq.EntityFramework.Tests && dnx -p test\Remote.Linq.EntityFramework.Tests test && dotnet pack src\Remote.Linq.EntityFramework --output artifacts --configuration Debug --version-suffix 001
+dotnet restore && dotnet build src\Remote.Linq.EntityFramework test\Remote.Linq.EntityFramework.Tests && dotnet test test\Remote.Linq.EntityFramework.Tests && dotnet pack src\Remote.Linq.EntityFramework --output artifacts --configuration Release --version-suffix 002
