25e85cb41bd1ae291c9582201a19fd20dc07fda7
6bee <christof.senn@gmail.com>
2018-02-27 00:59:01 +0100

    This git repo was integrated in 6bee/Remote.Linq

--- clean.bat
+++
@@ -1,2 +0,0 @@
-for /d /r . %%d in (artifacts,packages,bin,obj) do @if exist "%%d" rd /s /q "%%d"
-::del /s /q "*.xproj.user" "project.lock.json"
--- pack.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set configuration=Release
-set version-suffix=""
-clean ^
-  && dotnet restore ^
-  && dotnet build src\Remote.Linq.EntityFramework --configuration %configuration% ^
-  && dotnet build test\Remote.Linq.EntityFramework.Tests --configuration %configuration% ^
-  && dotnet test test\Remote.Linq.EntityFramework.Tests\Remote.Linq.EntityFramework.Tests.csproj --configuration %configuration% ^
-  && dotnet pack src\Remote.Linq.EntityFramework\Remote.Linq.EntityFramework.csproj --output "..\..\artifacts" --configuration %configuration% --include-symbols --version-suffix "%version-suffix%"
